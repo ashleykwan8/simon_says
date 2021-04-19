cd64eedf8a9ade17f73606387b39adbc7609e95b
Ashley Kwan <ashleykwan888@gmail.com>
2021-04-19 14:59:33 -0700

    added function to get a random sequence of colors

--- game.js
+++ game.js
@@ -1,4 +1,13 @@
+const buttonColors = ["red","blue","green","yellow"];
+let gamePattern = [];
+console.log(gamePattern);
+
 function nextSequence() {
     let randomNumber = Math.floor(Math.random() * 4);
     console.log(randomNumber);
+    let randomChosenColor = buttonColors[randomNumber];
+    console.log(randomChosenColor);
+    gamePattern.push(randomChosenColor);
 }
+
+
