243dae3a3758b6e48de0d1ef3194f3fe2c578d5f
Ashley Kwan <ashleykwan888@gmail.com>
2021-04-19 15:22:10 -0700

    added jquery flash animation

--- game.js
+++ game.js
@@ -2,12 +2,14 @@ const buttonColors = ["red","blue","green","yellow"];
 let gamePattern = [];
 console.log(gamePattern);
 
+
 function nextSequence() {
     let randomNumber = Math.floor(Math.random() * 4);
     console.log(randomNumber);
     let randomChosenColor = buttonColors[randomNumber];
     console.log(randomChosenColor);
     gamePattern.push(randomChosenColor);
+    $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
 }
 
 
