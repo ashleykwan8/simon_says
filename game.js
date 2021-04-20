dd04f1c47854235d6c0c90365ec287f10f18ea08
Ashley Kwan <ashleykwan888@gmail.com>
2021-04-20 11:49:09 -0700

    added startOver function to start over the game

--- game.js
+++ game.js
@@ -69,10 +69,15 @@ function checkAnswer(currentLevel) {
         console.log("Wrong!");
         $("body").addClass("game-over");
         setTimeout(function() {
-            $("body").removeClass("game-over")
+            $("body").removeClass("game-over");
         }, 200);
         $("#level-title").text("Game Over, Press Any Key to Restart");
+        startOver();
     }
 }
 
-
+function startOver() {
+    level = 0;
+    gamePattern = [];
+    started = false;
+}
